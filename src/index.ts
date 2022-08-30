845ce6d3329293b22d4c54b5972edda30f7b23b6
Abeer Ahmed <abeerahmad389@gmail.com>
2022-08-30 17:51:07 +0500

    MOSH

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+// enum size {Small =1, Medium,Large}
+// let mySize: size =size.Medium ;
+// console.log(mySize)
+
+// function calculateTax(income: number, taxYear:number):number{
+//     return 0;
+// }
+
+// function calculateTax(income: number, taxYear?:number):number{
+//     if((taxYear||2022)<2022)
+//     return income*1.2;
+//     return income*1.3;
+// }
+
+// console.log(calculateTax(10000))
+
+// type User={
+
+//     readonly id:number,
+//     name:string,
+//     retire:(date:Date)=>void
+
+// }
+// let user:User={id:1,name:"abeer",retire:(date:Date)=>{
+//     console.log(date)//can do anything here :D
+// }}
+
+// let user:
+
+// {   readonly id:number,
+//      name:string,
+//      retire:(date:Date)=>void
+
+// }={id:1,name:"abeer",retire:(date:Date)=>{
+//     console.log(date)//can do anything here :D 
+// }}
+
+
+// function kgtolbs(weight:number|string):number{
+// if(typeof(weight)==='number')
+// return weight*2.3;
+// return parseInt(weight)*2.5;
+// }
+
+// kgtolbs(10)
+// kgtolbs('10kg')
+
+// type Quantity = 50 |100;
+// let quantity : Quantity = 100;
+
+// function greet(name:String | null | undefined ){
+//     if(name)
+//     console.log(name.toUpperCase())
+//     else
+//     console.log("Hola!")
+// }
+
+// greet("Abeer");
+
+
+
+
+
+
